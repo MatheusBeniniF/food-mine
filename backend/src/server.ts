c58d279c3f3f38c46b91985c7e9e0e9e49062a9d
Matheus <matheus_benini@hotmail.com>
2025-02-09 16:54:34 -0300

    first commit

---
+++ backend/src/server.ts
@@ -0,0 +1,67 @@
+import express from 'express';
+import cors from 'cors';
+import { sample_foods, sample_tags, sample_users } from './data';
+import jwt from 'jsonwebtoken';
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:4200']
+}));
+
+app.get('/api/foods', (req, res) => {
+    res.send(sample_foods);
+});
+
+app.get('/api/foods/search/:searchTerm', (req, res) => {
+    const searchTerm = req.params.searchTerm;
+    const food = sample_foods.filter((food) =>
+        food.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    res.send(food);
+})
+
+app.get('/api/foods/tags', (req, res) => {
+    res.send(sample_tags);
+})
+
+app.get('/api/foods/tag/:tag', (req, res) => {
+    const tag = req.params.tag;
+    const foods = sample_foods.filter((food) => food.tags?.includes(tag));
+    res.send(foods);
+})
+
+app.get('/api/foods/:id', (req, res) => {
+    const foodId = req.params.id;
+    const food = sample_foods.find((food) => food.id === foodId);
+    res.send(food);
+})
+
+app.post('/api/users/login', (req, res) => {
+    const { email, password } = req.body;
+    const user = sample_users.find(
+        (user) => user.email === email && user.password === password
+    )
+
+    if (user) {
+        res.send(generateTokenResponse(user));
+    } else {
+        res.status(400).send('User name or password is wrong');
+    }
+})
+
+const generateTokenResponse = (user: any) => {
+    const token = jwt.sign({email: user.email, isAdmin: user.isAdmin}, 'SomeRandomText', {
+        expiresIn: '30d'
+    });
+    user.token = token;
+    return user;
+}
+
+const port = 5001;
+
+app.listen(port, () => {
+    console.log(`Website served on http://localhost:${port}`);
+});
